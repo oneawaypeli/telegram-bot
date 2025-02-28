f1834d84afb55fce939bc5f2ae5c91172dab46f5
oneawaypeli <fakelyfestyle@gmail.com>
2025-02-28 15:33:13 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,25 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
+
+
+TOKEN = 7803925257:AAGLxHgAPHJYgQLcGsYgsDpl_gvT-QavGmI
+
+async def start(update: Update, context):
+    await update.message.reply_text("Hei! Olen Telegram-botti. Kuinka voin auttaa?")
+
+async def echo(update: Update, context):
+    await update.message.reply_text(update.message.text)
+
+async def caps(update: Update, context):
+    text_caps = ' '.join(context.args).upper()
+    await update.message.reply_text(text_caps)
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("caps", caps))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
